a721523d79d764c23bcbde184f0c1509215a400d
Kevin Zepeda <kevinzepeda97@gmail.com>
2019-12-11 17:41:38 -0500

    Apex Legends

---
+++ client/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import VueToasted from 'vue-toasted';
+import App from './App.vue';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+Vue.use(VueToasted, {
+  iconPack: 'fontawesome',
+});
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app');
